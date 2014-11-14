3d6bb278a463f2e7fdfd1fca166b25945042f7e4
Katherine McClintic <kdmcclin@gmail.com>
2014-11-14 12:24:22 -0500

    Update user spec to remove password tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
 describe User do
-	let(:user1) {User.new(email: "santa@northpole.com", username: "santa", password: "northpole")}
-	let(:user2) {User.new(email: nil, username: "santa", password: "northpole")}
-	let(:user3) {User.new(email: "elf@northpole.com", username: nil, password: "iamanelf")}
-	let(:user4) {User.new(email: "mrsclaus@northpole.com", username: "mrsclaus", password: nil)}
-	let(:user5) {User.new(email: "elf@northpole.com", username: "thebigelfoncampus", password: "iamanelf")}
+	let(:user1) {User.new(email: "santa@northpole.com", username: "santa")}
+	let(:user2) {User.new(email: nil, username: "santa")}
+	let(:user3) {User.new(email: "elf@northpole.com", username: nil)}
+	let(:user4) {User.new(email: "elf@northpole.com", username: "thebigelfoncampus")}
 
-	it "is valid with an email, username, and password" do
+	it "is valid with an email and username" do
 		expect(user1).to be_valid
 	end
 
@@ -21,15 +20,10 @@ describe User do
 		expect(user3.errors[:username]).to include("can't be blank")
 	end
 
-	it "is invalid without a password" do
-		user4.valid?
-		expect(user4.errors[:password]).to include("can't be blank")
-	end
-
 	it "is invalid with a duplicate email address" do
-		User.create(email: "elf@northpole.com", username: "chiefelf", password: "iamanelf")
+		User.create(email: "elf@northpole.com", username: "chiefelf")
 
-		user5.valid?
-		expect(user5.errors[:email]).to include("has already been taken")
+		user4.valid?
+		expect(user4.errors[:email]).to include("has already been taken")
 	end
 end
